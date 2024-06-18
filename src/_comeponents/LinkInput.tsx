77467b9f99aad5cea9faa6a7c0dcfc0c3e1fca49
jacksonsophat <37005847+jacksonsophat@users.noreply.github.com>
2024-06-17 23:43:40 -0500

    update facebook card

--- src/_comeponents/LinkInput.tsx
+++ src/_comeponents/LinkInput.tsx
@@ -25,13 +25,13 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Facebook } from "lucide-react";
+import { Facebook, Linkedin } from "lucide-react";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 type SocialDataType = {
   title: string | "";
-  url: string | "";
+  url?: string | undefined;
   imageUrl: string | "";
   metaDescription: string | "";
 };
@@ -61,11 +61,14 @@ function LinkInput() {
 
     try {
       const data = await getURLInfomation(values.url);
-      console.log(data);
-      console.log("title", data.ogTitle);
+
+
+      const shortUrl = getHostname(data?.ogUrl);
+      console.log("shortUrl", shortUrl);
+
       const ogDataResult = {
         title: data.ogTitle || "",
-        url: data.ogUrl || "",
+        url: shortUrl || "",
         imageUrl: data.ogImage[0].url || "",
         metaDescription: data.ogDescription || "",
       };
@@ -78,6 +81,19 @@ function LinkInput() {
     } finally {
       setFetching(false); // Reset fetching state
     }
+  }
+
+  function getHostname(url: string) {
+    // Create a new URL object
+    const parsedUrl = new URL(url);
+    // Extract the hostname, split it by dots
+    const hostnameParts = parsedUrl.hostname.split('.');
+    // If there are more than one part, return the last two parts joined by a dot (domain.com)
+    if (hostnameParts.length > 1) {
+      return hostnameParts.slice(-2).join('.');
+    }
+    // Otherwise, return the entire hostname (if no subdomain)
+    return hostnameParts[0];
   }
 
   return (
@@ -111,27 +127,48 @@ function LinkInput() {
             <CardHeader>
               <CardTitle className={cn("flex")}>
                 <Facebook className="mr-2" />
-                Facebook {ogData?.title}
+                Facebook
               </CardTitle>
-              {/* <CardDescription>Card Description</CardDescription> */}
             </CardHeader>
             <CardContent>
-              {/* {ogData?.imageUrl} */}
-              {/* <Image
-                src={ogData?.imageUrl}
-                width={1000}
-                height={800}
-                alt="facebook image"
-              /> */}
-              <div className="aspect-video">
-                <img src={ogData?.imageUrl} />
+              <div>
+                <div className="aspect-video">
+                  <img src={ogData?.imageUrl} />
+                </div>
+                <div className="break-words border-[1px] border-[#dadde1] bg-[#f2f3f5] px-[12px] py-[10px] antialiased">
+                  <div className="overflow-hidden truncate whitespace-nowrap text-[12px] uppercase leading-[11px] text-[#606770]">
+                    {ogData?.url}
+                  </div>
+                  <div className="block h-[46px] max-h-[46px] border-separate select-none overflow-hidden break-words text-left">
+                    <div className="mt-[3px] truncate pt-[2px] text-[16px] font-semibold leading-[20px] text-[#1d2129]">{ogData?.title}</div>
+                    <div className="mt-[3px] block h-[18px] max-h-[80px] border-separate select-none overflow-hidden truncate whitespace-nowrap break-words text-left text-[14px] leading-[20px] text-[#606770]">
+                      {ogData?.metaDescription}
+                    </div>
+                  </div>
+                </div>
               </div>
             </CardContent>
-            <CardFooter className={cn("inline-block")}>
-              <p>website url</p>
-              <p>Pagetitle</p>
-              <p>Meta description</p>
-            </CardFooter>
+          </Card>
+        </section>
+        <section>
+          <Card>
+            <CardHeader>
+              <CardTitle className={cn("flex")}>
+                <Linkedin className="mr-2" />
+                LinkedIn
+              </CardTitle>
+            </CardHeader>
+            <CardContent >
+              <div className={cn('py-[1.2rem] px-[1.6rem] flex bg-[#edf3f8]')}>
+                <div className="aspect-video w-[128px] mr-2">
+                  <img src={ogData?.imageUrl} />
+                </div>
+                <div>
+                  <p className="text-[14px] overflow-hidden text-ellipsis text-wrap font-semibold">{ogData?.title}</p>
+                  <p className="mt-[0.8rem] overflow-hidden text-ellipsis text-black/60">{ogData?.url}</p>
+                </div>
+              </div>
+            </CardContent>
           </Card>
         </section>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import LinkInput from "@/_comeponents/LinkInput";
 
-export default function Home() {
+export default async function Home() {
+
+
+
   return (
     <main className="container my-8">
       <h1 className="mb-8">Social Preview</h1>
