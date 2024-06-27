eab22cc99ab1cf7f895f1d65bd748d23fd9db4cd
jacksonsophat <37005847+jacksonsophat@users.noreply.github.com>
2024-06-26 19:45:02 -0500

    na

--- src/app/api/social/route.ts
+++
@@ -1,8 +0,0 @@
-const GET = async (url: string) => {
-    // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    // const parsedResponse = await response.json();
-    return Response.json({ data: url, status: 200, message: 'Success' })
-}
-
-
-export { GET }
